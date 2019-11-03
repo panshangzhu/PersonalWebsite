a69c57d17bfe6debfc7bc125f53d8221616e12fd
panshangzhu <vincentwang677090@gmail.com>
2019-11-03 15:32:08 -0500

    add favicon

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
-const express=require('express');
-const cors=require('cors');
-const mongoose=require('mongoose');
-const path=require('path');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
 
-const app=express();
-const port=process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 //app.use(express.static(path.join(__dirname,'./myprofile/build')));
 
-const uri=process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology: true });
-const connection=mongoose.connection;
-connection.once('open',()=>{console.log("MongoDB data connected")});
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB data connected");
+});
+const CommentRouter = require("./CommentRouter");
+app.use("/comment", CommentRouter);
 
-const CommentRouter=require('./CommentRouter');
-app.use('/comment',CommentRouter);
-
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('myprofile/build'));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'myprofile','build','index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("myprofile/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "myprofile", "build", "index.html"));
+  });
+  const CommentRouter = require("./CommentRouter");
+  app.use("/comment", CommentRouter);
 }
 
-
-
-app.listen(port,()=>console.log(`Server is running on ${port}`));
+app.listen(port, () => console.log(`Server is running on ${port}`));
