cc126ceef4511e204247568ac1148d46bf14fab2
panshangzhu <yipan.wang677090@gmail.com>
2020-05-16 09:57:48 -0400

    images

--- myprofile/src/components/ProgrammingCarousel.js
+++ myprofile/src/components/ProgrammingCarousel.js
@@ -6,21 +6,23 @@ import html from "../assets/FrontEnd/html.png";
 import css from "../assets/FrontEnd/css.png";
 import js from '../assets/FrontEnd/js.png';
 import react from "../assets/FrontEnd/react.jpg";
+import materialUi from '../assets/FrontEnd/materialUi.png'
 import bootstrap from "../assets/FrontEnd/bootstrap.png";
 import webpack from "../assets/FrontEnd/webpack.png";
 import csharp from '../assets/BackEnd/csharp.jpg';
 import java from '../assets/BackEnd/java.png';
 import mvc from '../assets/BackEnd/mvc.png';
+import python from '../assets/BackEnd/python.jpg';
 import mongoDB from '../assets/FullStack/mongoDB.png';
 import node from '../assets/FullStack/node.png';
 import express from '../assets/FullStack/express.png';
 import oracle from '../assets/FullStack/oracle.png';
-
+import wp from '../assets/FullStack/wp.png'
 const Styles = styled.div`
 
   .carousel {
-    width: 60%;
-    height: 300px;
+    width: 65%;
+    height: 280px;
     background: rgba(0,0,0,0.3);
     margin-left: auto;
     margin-right: auto;
@@ -43,7 +45,7 @@ const Styles = styled.div`
     margin-left: 10%;
     display: grid;
     grid-template-columns: repeat(auto-fit,minmax(50px,1fr));
-    grid-row-gap: 15px;
+    grid-row-gap: 20px;
     justify-items: center;
   }
   img {
@@ -65,6 +67,8 @@ export default function ProgrammingCarousel() {
             <Image className="w-10 " src={react} alt="react" roundedCircle />
             <Image className="w-10 " src={bootstrap} alt="bootstrap" roundedCircle />
             <Image className="w-10 " src={webpack} alt="webpack" roundedCircle />
+            <Image className="w-10 " src={materialUi} alt="materialUi" roundedCircle />
+            <Image className="w-10 " src={wp} alt="wordpress" roundedCircle />
           </div>
         </Carousel.Item>
         <Carousel.Item>
@@ -73,16 +77,11 @@ export default function ProgrammingCarousel() {
             <Image className="w-10" src={csharp} alt="csharp" roundedCircle />
             <Image className="w-10 " src={java} alt="java" roundedCircle />
             <Image className="w-10 " src={mvc} alt="mvc" roundedCircle />
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <h3>Full Stack</h3>
-          <div id="images">
-            <Image className="w-10" src={mongoDB} alt="mongo" roundedCircle />
+            <Image className="w-10 " src={python} alt="python" roundedCircle />
             <Image className="w-10 " src={node} alt="node" roundedCircle />
             <Image className="w-10 " src={express} alt="express" roundedCircle />
+            <Image className="w-10" src={mongoDB} alt="mongo" roundedCircle />
             <Image className="w-10 " src={oracle} alt="oracle" roundedCircle />
-            
           </div>
         </Carousel.Item>
       </Carousel>
